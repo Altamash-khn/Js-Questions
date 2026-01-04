2d8c8db15b2bf4b60c7996d1939ba0e2500ffc3d
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-04 20:12:36 +0530

    Implement Fibonacci sequence generation up to N

--- Fibonacci sequence/fibonacci.js
+++ Fibonacci sequence/fibonacci.js
@@ -1,3 +1,4 @@
+// THIS WILL FIND THE FIRST 'N' NUMBERS IN THE FIBONACCI SEQUENCE
 function getFibonacciNumbers(num) {
   let fibonacciArr = [0, 1];
 
@@ -17,3 +18,22 @@ function getFibonacciNumbers(num) {
 }
 
 console.log(getFibonacciNumbers(10));
+
+// THIS WILL FIND ALL FIBONACCI NUMBERS LESS THAN OR EQUAL TO 'N'
+function fib(n) {
+  let fibArr = [0, 1];
+
+  while (fibArr[fibArr.length - 1] < n) {
+    let newFib = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
+
+    if (newFib <= n) {
+      fibArr.push(newFib);
+    } else {
+      break;
+    }
+  }
+
+  return fibArr;
+}
+
+console.log(fib(5));
