56692a7dd327c89dedd9760da9b0d9db0be0c717
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-06 14:49:41 +0530

    solved Nth number of fibonacci numbers using recursion

--- Fibonacci sequence/fibonacci.js
+++ Fibonacci sequence/fibonacci.js
@@ -1,23 +1,39 @@
 // THIS WILL FIND THE FIRST 'N' NUMBERS IN THE FIBONACCI SEQUENCE
-function getFibonacciNumbers(num) {
-  let fibonacciArr = [0, 1];
+// 1. using a loop to generate the Fibonacci sequence up to 'num' elements
+// function getFibonacciNumbers(num) {
+//   let fibonacciArr = [0, 1];
 
-  let num1 = 0;
-  let num2 = 1;
-  let nextNum = null;
+//   let num1 = 0;
+//   let num2 = 1;
+//   let nextNum = null;
 
-  for (let i = 2; i < num; i++) {
-    nextNum = num1 + num2;
-    fibonacciArr.push(nextNum);
-    num1 = num2;
-    num2 = nextNum;
-    nextNum = null;
+//   for (let i = 2; i < num; i++) {
+//     nextNum = num1 + num2;
+//     fibonacciArr.push(nextNum);
+//     num1 = num2;
+//     num2 = nextNum;
+//     nextNum = null;
+//   }
+
+//   return fibonacciArr;
+// }
+// console.log(getFibonacciNumbers(10));
+
+// 2. using recursion to generate the Fibonacci sequence up to 'num' elements
+function getFibNumbersTill(n, a = 0, b = 1, result = [0]) {
+  if (n <= 0) {
+    return "number should positive";
   }
 
-  return fibonacciArr;
+  if (result.length === n) {
+    return result;
+  }
+
+  result.push(b);
+  return getFibNumbersTill(n, b, b + a, result);
 }
 
-console.log(getFibonacciNumbers(10));
+console.log(getFibNumbersTill(9));
 
 // THIS WILL FIND ALL FIBONACCI NUMBERS LESS THAN OR EQUAL TO 'N'
 function fib(n) {
