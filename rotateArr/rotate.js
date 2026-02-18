5bfbcb7954eaec1e178c1f5cc28ad370ef2ebcbf
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-02-19 00:21:24 +0530

    implemented a better solution, avoid unnecessary iteration

--- rotateArr/rotate.js
+++ rotateArr/rotate.js
@@ -1,6 +1,22 @@
 // Solution 1
+// function rotate(arr, n) {
+//   let rotatedArr = [...arr];
+//   for (let i = 1; i <= n; i++) {
+//     let lastELEMENT = rotatedArr[rotatedArr.length - 1];
+
+//     rotatedArr.pop();
+//     rotatedArr.unshift(lastELEMENT);
+//   }
+
+//   return rotatedArr;
+// }
+// console.log(rotate([1, 2, 3, 4, 5, 6], 1));
+
+// Solution 2
 function rotate(arr, n) {
   let rotatedArr = [...arr];
+  let len = rotatedArr.length;
+  n = n % len;
   for (let i = 1; i <= n; i++) {
     let lastELEMENT = rotatedArr[rotatedArr.length - 1];
 
@@ -10,4 +26,4 @@ function rotate(arr, n) {
 
   return rotatedArr;
 }
-console.log(rotate([1, 2, 3, 4, 5, 6], 1));
+console.log(rotate([1, 2, 3, 4, 5, 6], 2));
