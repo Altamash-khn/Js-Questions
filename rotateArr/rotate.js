f5388f9a839e1e72121f22452c4f0f46d1d6b555
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-02-20 22:42:10 +0530

    solved without using a loop

--- rotateArr/rotate.js
+++ rotateArr/rotate.js
@@ -13,17 +13,24 @@
 // console.log(rotate([1, 2, 3, 4, 5, 6], 1));
 
 // Solution 2
+// function rotate(arr, n) {
+//   let rotatedArr = [...arr];
+//   let len = rotatedArr.length;
+//   n = n % len;
+//   for (let i = 1; i <= n; i++) {
+//     let lastELEMENT = rotatedArr[rotatedArr.length - 1];
+
+//     rotatedArr.pop();
+//     rotatedArr.unshift(lastELEMENT);
+//   }
+
+//   return rotatedArr;
+// }
+
 function rotate(arr, n) {
-  let rotatedArr = [...arr];
-  let len = rotatedArr.length;
+  let len = arr.length;
   n = n % len;
-  for (let i = 1; i <= n; i++) {
-    let lastELEMENT = rotatedArr[rotatedArr.length - 1];
 
-    rotatedArr.pop();
-    rotatedArr.unshift(lastELEMENT);
-  }
-
-  return rotatedArr;
+  return [...arr.slice(len - n), ...arr.slice(0, len - n)];
 }
 console.log(rotate([1, 2, 3, 4, 5, 6], 2));
