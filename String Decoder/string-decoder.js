ef1146e8c876fd7f86fede183e9a5bb74eba8aa5
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-02-21 21:51:14 +0530

    implemented a nested string's solution

--- String Decoder/string-decoder.js
+++ String Decoder/string-decoder.js
@@ -24,20 +24,58 @@
 // console.log(decodeString("3a2b1c"));
 
 // SOLUTION 2
-function xyz(str) {
-  let value = "";
+// function xyz(str) {
+//   let value = "";
 
-  let previousIndex = 0;
+//   let previousIndex = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (isNaN(+str[i])) {
+//       for (let j = 0; j < +str.slice(previousIndex, i); j++) {
+//         value += str[i];
+//       }
+//       previousIndex = i + 1;
+//     }
+//   }
+
+//   return value;
+// }
+
+// console.log(xyz("3a2b1c"));
+
+function test(str) {
+  let str2 = "";
   for (let i = 0; i < str.length; i++) {
-    if (isNaN(+str[i])) {
-      for (let j = 0; j < +str.slice(previousIndex, i); j++) {
-        value += str[i];
-      }
-      previousIndex = i + 1;
+    if (
+      (str[i] >= "a" && str[i] <= "z") ||
+      (str[i] >= "A" && str[i] <= "Z") ||
+      !isNaN(str[i])
+    ) {
+      str2 += str[i];
     }
   }
 
-  return value;
+  return str2;
 }
 
-console.log(xyz("3a2b1c"));
+function decodeString(s) {
+  let stringDecode = test(s);
+  let decodedString = "";
+  let number = "";
+
+  for (let i = stringDecode.length - 1; i >= 0; i--) {
+    if (!isNaN(stringDecode[i])) {
+      number = stringDecode[i] + number;
+    } else {
+      decodedString = decodedString.repeat(+number);
+      number = "";
+      decodedString = stringDecode[i] + decodedString;
+    }
+  }
+  return decodedString.repeat(+number);
+}
+
+console.log(decodeString("2[a3[b2[c]]]"));
+console.log(decodeString("2[ab]"));
+console.log(decodeString("10[a]"));
+console.log(decodeString("3[ab2[c]]"));
+console.log(decodeString("3a4b2c"));
