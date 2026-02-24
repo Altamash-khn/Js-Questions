ac1c37e4eb6a504b0681a25539ec86894fc9221d
Altamash <Altamashkhn281@gmail.com>
2026-02-24 08:58:33 +0530

    Refactor sumAllDigits and implement recursion
    Refactor sumAllDigits function and add recursive solution.

--- sumOfAllDigits/sumAll.js
+++ sumOfAllDigits/sumAll.js
@@ -1,13 +1,18 @@
 // Find the sum of digits of a number.
 
-function sumAllDigits(num) {
-  let arr = Array.from(String(num), Number);
-  return arr.reduce(function (acc, currVal) {
-    acc += currVal;
-    return acc;
-  });
-}
+// SOLUTION 1
+// function sumAllDigits(num) {
+//   let str = num.toString();
+//   let arr = str.split("");
+//   let sumOfAllDigits = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sumOfAllDigits += +arr[i];
+//   }
+//   return sumOfAllDigits;
+// }
 
+
+// SOLUTION 2
 // function sumAllDigits(num) {
 //   let str = num.toString();
 //   let finalStr = "";
@@ -18,12 +23,24 @@ function sumAllDigits(num) {
 //   return eval(finalStr);
 // }
 
+// SOLUTION 3
 // function sumAllDigits(num) {
-//   let str = num.toString();
-//   let arr = str.split("");
-//   let sumOfAllDigits = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     sumOfAllDigits += +arr[i];
-//   }
-//   return sumOfAllDigits;
+//   let arr = Array.from(String(num), Number);
+//   return arr.reduce(function (acc, currVal) {
+//     acc += currVal;
+//     return acc;
+//   });
 // }
+
+function sumOfDigitsRecursive(num) {
+    if (!num) {
+        return 0;
+    }
+    
+    num = num.toString()
+    
+    return parseInt(num[num.length -1]) 
+        + parseInt(sumOfDigitsRecursive(num.slice(0, num.length -1)));
+}
+
+console.log(sumOfDigitsRecursive(1234))
